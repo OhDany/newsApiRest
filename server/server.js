8e78f7f755e61b5d6b06e06d4e6936e691a13b9a
Daniel Ortega <oh_dany@hotmail.com>
2018-10-09 20:54:43 -0500

    Usuario con el metodo post

--- server/server.js
+++ server/server.js
@@ -13,6 +13,25 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Models
+const { User } = require('./models/user')
+
+//<><><><><><><><><><><><>
+//         Users
+//<><><><><><><><><><><><>
+app.post('/api/users/register',(req, res)=>{
+    const user = new User(req.body);
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success: true,
+            userdata: doc
+        })
+    })
+});
+
+
 const port = process.env.port || 3002;
 
 app.listen(port,() => {
